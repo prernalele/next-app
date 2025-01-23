04580603b0375ae0be887ed8ce511905374abff0
Prerna Lele <prernalele@gmail.com>
2025-01-23 16:37:15 -0500

    feat: put and delete methods

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -19,3 +19,32 @@ export async function GET(
   });
 }
 
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  }
+  if (params.id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  //Fetch user from db
+  // If user is not found
+  //we return 404
+  // if found we delete the user
+  // return 200
+  if (params.id > 10) {
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  }
+  return NextResponse.json({});
+}
+
